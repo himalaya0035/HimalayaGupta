e558642ac89cbce61b3f50705e8b270c0f78336a
himalaya0035 <61822515+himalaya0035@users.noreply.github.com>
2020-08-28 14:40:21 +0530

    first commit

---
+++ js/style.js
@@ -0,0 +1,9 @@
+// sections = document.getElementsByTagName("section");
+// for (var j = 0; j < sections.length; j++) {
+//     console.log(sections[j])
+//     sections[j].addEventListener('dblclick',darkMode)
+// }
+// count = 0;
+// function darkMode() {
+
+// }
